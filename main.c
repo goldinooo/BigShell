15ab0f800021d694336f8b34f3e28c59630cd5bb
Golden <redathr2018@gmail.com>
2025-09-30 23:28:32 -0700

    fixed conf

--- main.c
+++ main.c
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-#include "minishell.h"
-
-
-void	process_line(char *line/*Takes the shell struct*/)
-{
-	// TODO:
-}
-
-void	interactive_mode(/*Takes the shell struct*/)
-{
-
-}
-
-void	script_mode(int fd/*Takes the shell struct*/)
-{
-	char	*line;
-
-	while (true)
-	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		// TODO: process line
-	}
-}
-
-int main(int ac, char **av)
-{
-	// init & process the envs
-	t_shell	shell;
-
-	shell.env = init_env();
-	if (isatty(STDIN_FILENO))
-		interactive_mode(/*Takes the shell struct*/);
-	else
-		script_mode(STDIN_FILENO/*Takes the shell struct*/);
-	return (0);
-}
-
-=======
 #include "lexer.h"
 #include "minishell.h"
 #include "utils.h"
@@ -83,7 +42,9 @@ void	script_mode(int fd/*Takes the shell struct*/)
 int main(int ac, char **av)
 {
 	// init & process the envs
+	t_shell	shell;
 
+	shell.env = init_env();
 	if (isatty(STDIN_FILENO))
 		interactive_mode(/*Takes the shell struct*/);
 	else
@@ -91,4 +52,3 @@ int main(int ac, char **av)
 	return (0);
 }
 
->>>>>>> refs/remotes/origin/lexer
