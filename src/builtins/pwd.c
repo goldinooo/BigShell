20be9cff8ab533b177c29877f20021d16e1f9e9e
Golden <redathr2018@gmail.com>
2025-10-03 01:13:29 -0700

    final parsing

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -1,6 +1,22 @@
 #include "minishell.h"
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void ex_pwd(t_shell *shell)
 {
-	
+	char cwd[1024];
+
+	if(getcwd(cwd, sizeof(cwd)))
+	{
+		ft_putstr_fd(cwd, 1);
+		ft_putstr_fd("\n", 1);
+		shell->exit_status = EXIT_SUCCESS;
+	}
+	else 
+	{
+		perror("invalid pwd behavior");
+		shell->exit_status = EXIT_SUCCESS;
+	}
 }
